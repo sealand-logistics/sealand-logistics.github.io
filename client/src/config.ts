e2f7ec6596ad9bc310de46013457cd7273fced02
sealand-logistics <sealandshipping.bbroy@gmail.com>
2026-03-06 10:10:55 +0530

    Fix configuration to support domain-based API calls and images

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,3 +1,5 @@
 export const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     ? 'http://localhost:5000/api'
-    : 'http://187.77.184.78:5000/api';
+    : (window.location.hostname === '187.77.184.78'
+        ? 'http://187.77.184.78:5000/api'
+        : `https://${window.location.hostname}/api`);
